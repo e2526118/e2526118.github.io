129db04bd345d69ac1e17313aa7cc36d8c2a6ccc
e2526118 <e2526118@soka-u.jp>
2026-07-24 12:22:19 +0900

    最終課題

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
-console.log("Hello World !");
-console.log("Hello World !!");
-console.log("Hello World !!!");
-console.log("Hello World !!!!");
-console.log("Hello World !!!!!");
+function janken(myHand) {
 
-function sayspring(language) {
+    let handArray = ['ぐー', 'ちょき', 'ぱー'];
 
-    message = "春";
-    if (language === 0) {
-        console.log("春");
-    } else if (language === 1) {
-        message = "spring";
-    } else if (language === 2) {
-        message = "printemps";
-    } else if (language === 3) {
-        message = "primavera";
-    } else if (language === 4) {
-        message = "春天";
-    }
+    let resultArray = ['アイコです．', 'あなたの負けです．', 'あなたの勝ちです．'];
 
-    console.log(message);
 
-    const spring_result = document.querySelector('#say_spring');
-    spring_result.textContent = message;
+    let computerHand = 0;
 
+
+    let result = 0;
+
+
+    console.log('あなたの手は：' + handArray[myHand]);
+
+
+    let rand = Math.random() * 100;
+    console.log('乱数：' + rand);
+
+
+    let rand_int = Math.floor(rand);
+    console.log('乱数（整数）：' + rand_int);
+
+
+    computerHand = rand_int % 3;
+    console.log('computerHand：' + computerHand);
+    console.log('コンピュータの手は：' + handArray[computerHand]);
+
+    result = (myHand - computerHand + 3) % 3
+    
+
+    console.log(resultArray[result]);
+    console.log('-----------------------------');
+
+    const janken_result = document.querySelector('#result');
+    janken_result.textContent = resultArray[result];
 }
